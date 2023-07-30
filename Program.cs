492d5260f22f01249359a804807176540e54e061
Igor Dudin <dudinio@mail.ru>
2023-07-30 13:23:14 +0300

    Работающий код

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите число 1: ");
+string strNum1=Console.ReadLine();
+
+Console.WriteLine("Введите число 2: ");
+string strNum2=Console.ReadLine();
+
+int num1=int.Parse(strNum1);
+int num2=int.Parse(strNum2);
+
+if(num1>num2)
+{
+    Console.WriteLine($"Число {num1} больше числа {num2}");
+}
+else if (num2>num1)
+{
+    Console.WriteLine($"Число {num2} больше числа {num1}");
+}
+else
+{
+    Console.WriteLine($"Число {num1} равно числу {num2}");
+}
